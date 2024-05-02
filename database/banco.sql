874ce85eced61c3ed78571c2099c8e67957a9469
Rodrigo Borges <161477308+Grupo-015@users.noreply.github.com>
2024-05-02 12:12:57 -0300

    Update banco.sql

--- database/banco.sql
+++ database/banco.sql
@@ -1,84 +1,20 @@
--- CREATE DATABASE banco;
+CREATE SCHEMA `databasecookies` ;
 
--- USE banco;
-
-CREATE TABLE pedidos (
-    
-    id INT NOT NULL AUTO_INCREMENT,
-    
-    -- Dados do Comprador
-    nomeComprador TEXT,
-    emailComprador TEXT,
-    telefoneComprador TEXT,
-
-    -- Menu
-    cookieTradicionalGrande TEXT,
-    cookieTradicionalMini TEXT,
-    cookieRecheadoDoceDeLeite TEXT,
-    cookieRecheadoNutella TEXT,
-    tortaDeCookieCarameloENutella TEXT,
-    tortaDeCookieNutella TEXT,
-
-    -- Dados da Entrega
-    ruaEntrega TEXT,
-    numeroEntrega TEXT,
-    cepEntrega TEXT,
-    estadoEntrega TEXT,
-    cidadeEntrega TEXT,
-    dataEntrega TEXT,
-
-    -- Valor
-    valorTotal TEXT,
-
-    
-    PRIMARY KEY (id)
-    
-) DEFAULT CHARSET = utf8;
-
-INSERT INTO pedidos (
-    
-    nomeComprador, 
-    emailComprador, 
-    telefoneComprador,
-
-    cookieTradicionalGrande,
-    cookieTradicionalMini,
-    cookieRecheadoDoceDeLeite,
-    cookieRecheadoNutella,
-    tortaDeCookieCarameloENutella,
-    tortaDeCookieNutella,
-
-    ruaEntrega,
-    numeroEntrega,
-    cepEntrega,
-    estadoEntrega,
-    cidadeEntrega,
-    dataEntrega,
-
-    valorTotal
-    
-    ) 
-    
-VALUES (
-    
-    "nome", 
-    "email", 
-    "telefone",
-    
-    "cookieTradicionalGrande",
-    "cookieTradicionalMini",
-    "cookieRecheadoDoceDeLeite",
-    "cookieRecheadoNutella",
-    "tortaDeCookieCarameloENutella",
-    "tortaDeCookieNutella",
-
-    "rua-entrega",
-    "numero-entrega",
-    "cep-entrega",
-    "estado-entrega",
-    "cidade-entrega",
-    "dat-entrega",
-
-    "valorTotal"
-    
-    );
+CREATE TABLE `databasecookies`.`pedidos` (
+  `nomeComprador` VARCHAR(90) NULL,
+  `emailComprador` VARCHAR(45) NULL,
+  `telefoneComprador` VARCHAR(45) NULL,
+  `cookieTradicionalGrande` VARCHAR(45) NULL,
+  `cookieTradicionalMini` VARCHAR(45) NULL,
+  `cookieRecheadoDoceDeLeite` VARCHAR(45) NULL,
+  `cookieRecheadoNutella` VARCHAR(45) NULL,
+  `tortaDeCookieCarameloENutella` VARCHAR(45) NULL,
+  `tortaDeCookieNutella` VARCHAR(45) NULL,
+  `valorTotal` VARCHAR(45) NULL,
+  `ruaEntrega` VARCHAR(45) NULL,
+  `numeroEntrega` VARCHAR(45) NULL,
+  `cepEntrega` VARCHAR(45) NULL,
+  `estadoEntrega` VARCHAR(45) NULL,
+  `cidadeEntrega` VARCHAR(45) NULL,
+  `dataEntrega` VARCHAR(45) NULL,
+  `observacoes` VARCHAR(45) NULL);
